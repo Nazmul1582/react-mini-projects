abe14da517ced9dd1bd4637c6641cb53cdbe71dc
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-02-08 16:08:41 +0600

    created a Bio Data App

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,36 +1,38 @@
-const PersonalInfo = ({ person }) => {
+const PersonalInfo = ({ personalInfo }) => {
   return (
     <div className="flex flex-col-reverse sm:flex-row items-center justify-center sm:justify-between gap-5 mb-5">
       <div>
-        <h2 className="text-2xl font-bold text-white">{person.name}</h2>
+        <h2 className="text-2xl font-bold text-white">{personalInfo.name}</h2>
         <h3 className="text-lg font-semibold text-slate-300">
-          {person.profession}
+          {personalInfo.profession}
         </h3>
-        <h3 className="mb-4  font-semibold text-slate-300">{person.address}</h3>
+        <h3 className="mb-4  font-semibold text-slate-300">
+          {personalInfo.address}
+        </h3>
         <ul>
-          <li>Phone: {person.contact.phone}</li>
+          <li>Phone: {personalInfo.contact.phone}</li>
           <li>
             Email:{" "}
             <a href="" className="text-blue-500 underline">
-              {person.contact.email}{" "}
+              {personalInfo.contact.email}{" "}
             </a>{" "}
           </li>
           <li>
             Github:{" "}
             <a href="" className="text-blue-500 underline">
-              {person.contact.github}{" "}
+              {personalInfo.contact.github}{" "}
             </a>{" "}
           </li>
           <li>
             Website:{" "}
             <a href="" className="text-blue-500 underline">
-              {person.contact.website}{" "}
+              {personalInfo.contact.website}{" "}
             </a>{" "}
           </li>
         </ul>
       </div>
       <img
-        src={person.image}
+        src={personalInfo.image}
         alt="profilePic"
         className="w-48 rounded-lg object-cover shadow-xl shadow-slate-500/50"
       />
