adc47d7b45fb301e03d3e401caf06d1dc7c5b5b7
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-02-10 19:55:53 +0600

    created a Bio Data App

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,36 +1,27 @@
-const PersonalInfo = ({ person }) => {
+const PersonalInfo = ({ name, profession, address, contact, image }) => {
   return (
     <div className="flex flex-col-reverse sm:flex-row items-center justify-center sm:justify-between gap-5 mb-5">
       <div>
-        <h2 className="text-2xl font-bold text-white">{person.name}</h2>
-        <h3 className="text-lg font-semibold text-slate-300">
-          {person.profession}
-        </h3>
-        <h3 className="mb-4  font-semibold text-slate-300">{person.address}</h3>
-        <ul>
-          <li>Phone: {person.contact.phone}</li>
-          <li>
-            Email:{" "}
-            <a href="" className="text-blue-500 underline">
-              {person.contact.email}{" "}
-            </a>{" "}
-          </li>
-          <li>
-            Github:{" "}
-            <a href="" className="text-blue-500 underline">
-              {person.contact.github}{" "}
-            </a>{" "}
-          </li>
-          <li>
-            Website:{" "}
-            <a href="" className="text-blue-500 underline">
-              {person.contact.website}{" "}
-            </a>{" "}
-          </li>
-        </ul>
+        <h2 className="text-2xl font-bold text-white">{name}</h2>
+        <h3 className="text-lg font-semibold text-slate-300">{profession}</h3>
+        <h3 className="mb-4  font-semibold text-slate-300">{address}</h3>
+        {contact.map((ele) => (
+          <ul key={ele.value} className="flex">
+            <li>{ele.name}</li> :
+            <li
+              className={
+                ele.name === "phone"
+                  ? "ml-2"
+                  : "text-blue-500 underline ml-2 cursor-pointer"
+              }
+            >
+              {ele.value}
+            </li>
+          </ul>
+        ))}
       </div>
       <img
-        src={person.image}
+        src={image}
         alt="profilePic"
         className="w-48 rounded-lg object-cover shadow-xl shadow-slate-500/50"
       />
