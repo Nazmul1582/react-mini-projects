048e07304051503041628528d418dbf00004ced9
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-02-12 21:15:37 +0600

    created multiple counter

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const increment1Handler = () => setCount((prevState) => prevState + 1);
-  const increment10Handler = () => setCount((prevState) => prevState + 10);
-  const increment100Handler = () => setCount((prevState) => prevState + 100);
-
-  const decrement1Handler = () => setCount((prevState) => prevState - 1);
-  const decrement10Handler = () => setCount((prevState) => prevState - 10);
-  const decrement100Handler = () => setCount((prevState) => prevState - 100);
+  const incrementHandler = (value) =>
+    setCounter((prevState) => prevState + value);
+  const decrementHandler = (value) =>
+    setCounter((prevState) => prevState - value);
 
   return (
     <div className="h-screen bg-gradient-to-r from-cyan-500 to-pink-500 flex items-center justify-center">
       <div className="w-[500] bg-white shadow-xl p-5 rounded-xl text-center font-bold">
         <h1 className="text-2xl font-bold">Multiple Counter</h1>
-        <h2 className="text-4xl my-3">{count}</h2>
+        <h2 className="text-4xl my-3">{counter}</h2>
         <div className="grid grid-cols-3">
           <button
-            onClick={increment1Handler}
+            onClick={() => incrementHandler(1)}
             className="btn bg-cyan-500 shadow-cyan-500/50"
           >
             Increment By 1
           </button>
           <button
-            onClick={increment10Handler}
+            onClick={() => incrementHandler(10)}
             className="btn bg-pink-500 shadow-pink-500/50"
           >
             Increment By 10
           </button>
           <button
-            onClick={increment100Handler}
+            onClick={() => incrementHandler(100)}
             className="btn bg-indigo-500 shadow-indigo-500/50"
           >
             Increment By 100
           </button>
           <button
-            onClick={decrement1Handler}
+            onClick={() => decrementHandler(1)}
             className="btn bg-cyan-500 shadow-cyan-500/50"
           >
             Decrement By 1
           </button>
           <button
-            onClick={decrement10Handler}
+            onClick={() => decrementHandler(10)}
             className="btn bg-pink-500 shadow-pink-500/50"
           >
             Decrement By 10
           </button>
           <button
-            onClick={decrement100Handler}
+            onClick={() => decrementHandler(100)}
             className="btn bg-indigo-500 shadow-indigo-500/50"
           >
             Decrement By 100
